1c7fd46a2314db21a53277abe35e44696e837e3b
spestana <spestana@uw.edu>
2020-03-09 13:34:04 -0700

    June 2017 - I need a better way of doing this in the future

--- download-goes.py
+++ download-goes.py
@@ -15,15 +15,16 @@ bucket = 'noaa-goes16'
 satellite = bucket[5:] # get the last part of the bucket name
 # Specify date, time, product, band (channel)
 year='2017'
-month='05'
+month='06'
 start_day = 1
-stop_day = 31
+stop_day = 30
 days=np.linspace(start_day,stop_day,stop_day-start_day+1,dtype=np.int16)
 hours=['00','01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23']
 product='ABI-L1b-RadC'
 channel='C14' # 11.2 micron channel, "Longwave window"
 # Local paths where data will be stored
-homepath = '/storage/GOES' #str(os.path.expanduser("~")) # home directory
+homepath = '/storage/GOES' #check if files already exist in storage directory 
+#homepath = str(os.path.expanduser("~")) # home directory
 filepath = []; # store filepaths of the files we download
 
 
