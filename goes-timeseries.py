d8f68c55b750ea73a56d3dae55e049c0d9e1e1df
spestana <spestana@uw.edu>
2020-12-31 18:11:55 -0800

    Update script to work with any of the GOES ABI emissive bands and add outfile path option

--- goes-timeseries.py
+++ goes-timeseries.py
@@ -7,7 +7,7 @@ import sys, argparse
 
 
 ##############################################################
-def getListOfFiles(dirName):
+def getListOfFiles(dirName, channel):
     # create a list of file and sub directories 
     # names in the given directory 
     # https://thispointer.com/python-how-to-get-list-of-files-in-directory-and-sub-directories/
@@ -19,9 +19,9 @@ def getListOfFiles(dirName):
         fullPath = os.path.join(dirName, entry)
         # If entry is a directory then get the list of files in this directory 
         if os.path.isdir(fullPath):
-            allFiles = allFiles + getListOfFiles(fullPath)
+            allFiles = allFiles + getListOfFiles(fullPath,channel)
         else:
-            if 'C14' in fullPath:
+            if channel in fullPath:
                 allFiles.append(fullPath)
                 
     return allFiles 
@@ -61,13 +61,13 @@ def LonLat2ABIangle(lon_deg, lat_deg, z, H, req, rpol, e, lon_0_deg):
         return (x,y)
 
 ##############################################################    
-def goesBrightnessTemp(rad): 
-    # for GOES emissive bands (only for band 14 right now)
+def goesBrightnessTemp(rad, fk1, fk2, bc1, bc2): 
+    # for GOES emissive bands
     # TODO: Update this to work with the other emissive bands
-    fk1 = 8.51022e+03
-    fk2 = 1.28627e+03
-    bc1 = 0.22516
-    bc2 = 0.99920
+    #fk1 = 8.51022e+03
+    #fk2 = 1.28627e+03
+    #bc1 = 0.22516
+    #bc2 = 0.99920
     T = ( fk2 / (np.log((fk1 / rad) + 1)) - bc1 ) / bc2
     return T
 
@@ -76,16 +76,27 @@ def goesBrightnessTemp(rad):
 # Set up argument and error handling
 parser = argparse.ArgumentParser(description='Produces a timeseries of GOES ABI Radiance observations for a single location given a directory of GOES ABI files')
 parser.add_argument('-d','--dir', required=True, help='directory to search for GOES ABI files (.nc)')
+parser.add_argument('-c','--channel', required=True, type=str, help='GOES ABI channel/band (e.g. C14)')
 parser.add_argument('-l','--loc', required=True, nargs=3, type=float, help='Latitude, Longitude, and elevation of location to extract timeseries for')
+parser.add_argument('-o','--ofile', required=True, nargs=1, type=str, help='Output filepath and name')
 args = parser.parse_args()
 
 #-----------------------SET ARGUMENTS TO VARIABLES----------------------------#
+# Input directory
 indir = args.dir
+
+# GOES ABI Channel
+channel = args.channel
 
 # Lat, Lon, Elev.
 lat_obs = args.loc[0]
 lon_obs = args.loc[1]
 z_obs = args.loc[2]
+
+# Output filepath/filename
+out_filepath_filename = args.ofile[0]
+
+print(out_filepath_filename)
 
 
 
@@ -95,7 +106,7 @@ print('Load all observations from the directory provided')
 path = indir
 file_list = []
 try:
-    file_list.append(getListOfFiles(path))
+    file_list.append(getListOfFiles(path,channel))
 except FileNotFoundError:
     print('Could not find files at {}'.format(path))
 
@@ -126,7 +137,7 @@ with xr.open_dataset(file_list[0]) as f:
     H = f.goes_imager_projection.perspective_point_height + f.goes_imager_projection.semi_major_axis
     lon_0 = f.goes_imager_projection.longitude_of_projection_origin
     e = 0.0818191910435 # GRS-80 eccentricity
-    
+       
     # find corresponding look angles
     x_rad, y_rad = LonLat2ABIangle(lon,lat,z,H,req,rpol,e,lon_0)
     print('Extracting radiance values for: {}, {}'.format(x_rad, y_rad))
@@ -135,26 +146,35 @@ with xr.open_dataset(file_list[0]) as f:
 # Take a look at how radiance changes at this single point
 print('Take a look at how radiance changes at this single point')
 values = []
+Tb = []
 time = []
 for filename in file_list:
     with xr.open_dataset(filename) as f:
         # find corresponding pixel Radiance value nearest to these angles
         value = f.Rad.sel(y=y_rad, x=x_rad, method='nearest').values.mean()
         values.append(value)
+        # convert Radiance to Tb
+        # Values needed for radiometric conversion
+        fk1 = f.planck_fk1.values
+        fk2 = f.planck_fk2.values
+        bc1 = f.planck_bc1.values
+        bc2 = f.planck_bc2.values
+        # calculate brightness temperature for this band (K)
+        Tb.append(goesBrightnessTemp(value, fk1, fk2, bc1, bc2))
         start_time = f.time_bounds.values.min()
         time.append(start_time)
 
 
 ##############################################################
 
-print('Convert values, make dataframe')
+print('Make dataframe')
 # values are in mW for the emissive bands
 radiance = np.array(values)
-# calculate brightness temperature for this band, convert from K to C
-Tb = goesBrightnessTemp(radiance) - 273.15
+# convert from K to C for brightness temperature
+Tb = np.array(Tb) - 273.15
 
 # make a pandas dataframe out of this
-d = {'time': time, 'tb': Tb}
+d = {'time': time, 'rad': radiance, 'tb': Tb}
 df = pd.DataFrame(d)
 print(df)
 print(type(df))
@@ -162,4 +182,5 @@ print(type(df))
 ##############################################################
 # save this informaiton out to a pickle file
 print('save this informaiton out to a pickle file')
-df.to_pickle('goes-output.pkl', protocol=3)
+print(out_filepath_filename)
+df.to_pickle(out_filepath_filename, protocol=3)
