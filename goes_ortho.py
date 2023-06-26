29667c05f7a7cc7134d8611a1ff11134adbda9e4
spestana <spestana@uw.edu>
2023-06-26 15:46:25 -0700

    Remove old and redundant functions

--- goes_ortho.py
+++ goes_ortho.py
@@ -2,17 +2,13 @@
 Functions to orthorectify GOES-R ABI images using a DEM
 """
 
-
 import numpy as np
-import pandas as pd
 import xarray as xr
 import os
-import glob
-from asp_binder_utils import get_dem, run_bash_command
+from asp_binder_utils import get_dem
 from goes_geometry import LonLat2ABIangle
 from goes_rad import goesBrightnessTemp, goesReflectance
 
-  
 
 def ABIpixelMap(abi_grid_x, abi_grid_y):
     '''
@@ -38,11 +34,7 @@ def ABIpixelMap(abi_grid_x, abi_grid_y):
     #center_row = (np.floor(y_px))
 
     return center_x, center_y#, center_col, center_row
-
-
-
-
-    
+ 
 
 def make_ortho_map(goes_filepath, dem_filepath, out_filepath=None):
     '''For the entire DEM, determine the ABI scan angle coordinates for every DEM grid cell, 
@@ -148,46 +140,6 @@ def make_ortho_map(goes_filepath, dem_filepath, out_filepath=None):
     
     return ds
 
-def orthorectify_abi_rad(goes_filepath, pixel_map, out_filename=None):
-    '''Using the pixel mapping for a specific ABI viewing geometry over a particular location,
-    orthorectify the ABI radiance values and return an xarray dataarray with those values.'''
-    print('\nRUNNING: orthorectify_abi_rad()')
-    
-    # First check, Does the projection info in the image match our mapping?
-    print('\nDoes the projection info in the image match our mapping?')
-    # Open the GOES ABI image
-    print('\nOpening GOES ABI image...\t\t\tABI image value\tPixel map value')
-    abi_image = xr.open_dataset(goes_filepath)
-    print('perspective_point_height + semi_major_axis:\t{}\t{}'.format(abi_image.goes_imager_projection.perspective_point_height 
-                                                                       + abi_image.goes_imager_projection.semi_major_axis,
-                                                          pixel_map.satellite_height))
-    print('semi_major_axis:\t\t\t\t{}\t{}'.format(abi_image.goes_imager_projection.semi_major_axis,
-                                                          pixel_map.semi_major_axis))
-    print('semi_minor_axis:\t\t\t\t{}\t{}'.format(abi_image.goes_imager_projection.semi_minor_axis,
-                                                          pixel_map.semi_minor_axis))
-    print('longitude_of_projection_origin:\t\t\t{}\t\t{}'.format(abi_image.goes_imager_projection.longitude_of_projection_origin,
-                                                          pixel_map.longitude_of_projection_origin))
-    print('...done')
-    
-    # Map (orthorectify) and clip the image to the pixel map
-    print('\nMap (orthorectify) and clip the image to the pixel map')
-    abi_rad_values = abi_image.sel(x=pixel_map.dem_px_angle_x, y=pixel_map.dem_px_angle_y, method='nearest').Rad.values
-    print('...done')
-	
-    # Output this result to a new NetCDF file
-	#Create a new xarray dataset with the orthorectified ABI radiance values, 
-    #Lat, Lon, Elevation, and metadata from the pixel map. 
-    print('\nOutput this result to a new NetCDF file')
-    if out_filename == None:
-        out_filename=abi_image.dataset_name+'_ortho.nc'
-    print('Saving file as: {}'.format(out_filename))
-    pixel_map['rad'] = (['latitude', 'longitude'], abi_rad_values)
-    pixel_map['tb'] = goesBrightnessTemp(pixel_map.rad, abi_image.planck_fk1.values, abi_image.planck_fk2.values, abi_image.planck_bc1.values, abi_image.planck_bc2.values)
-    pixel_map.to_netcdf(out_filename)
-    print('...done')
-    
-    return pixel_map
-
 
 def orthorectify_abi(goes_filepath, pixel_map, data_vars, out_filename=None):
     '''Using the pixel mapping for a specific ABI viewing geometry over a particular location,
@@ -291,23 +243,3 @@ def ortho(goes_image_path, data_vars, bounds, api_key, new_goes_filename, dem_fi
         os.remove(dem_filepath)
     
     return None
-
-def output_ortho_netcdf(abi_rad_values, pixel_map, out_filename):
-
-    print('\nRUNNING: output_ortho_netcdf()')
-    
-    # some metadata for this
-    metadata = {'rad' : 'units'}
-    
-    # make the data array
-    #rad_da = xr.DataArray(abi_rad_values, 
-    #                      dims=('y','x'),
-    #                      coords={'latitude': (['y'], pixel_map.latitude),
-    #                            'longitude': (['x'], pixel_map.longitude)},
-    #                     attrs=metadata)
-
-    
-    return None
-
-
-
