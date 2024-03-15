e574832544fd3b047e87c881920140832fe378e5
spestana <spestana@uw.edu>
2024-03-15 11:53:26 -0700

    Add geom test

---
+++ tests/test_geometry.py
@@ -0,0 +1,54 @@
+'''Functions for testing goes_geometry module.'''
+
+from os import path
+import sys
+from pathlib import Path
+import xarray as xr
+from goes_ortho import goes_geometry
+from goes_ortho import get_data
+
+### test functions for CONUS images ###
+
+def goes_clip_conus():
+    filepath = './resources/spestana-goes-ortho-data-bc4e02e/data/C/'
+    bounds = [30, 40, -110, -100]
+    newfilepath = path.abspath('./tmp.nc')
+    goes_clip.subsetNetCDF(filepath,bounds,newfilepath)
+
+def test_goes_clip_conus():
+    # run goes clip function
+    goes_clip_conus()
+    # do we have a dataset?
+    assert type(xr.open_dataset('tmp.nc')) == xr.core.dataset.Dataset, "unable to open file"
+    ds = xr.open_dataset('tmp.nc')
+    # does it have non-zero dimensions?
+    assert ds.dims['y'] != 0, "y dimension should be non-zero, check the latitude bounds you've provided"
+    assert ds.dims['x'] != 0, "x dimension should be non-zero, check the longitude bounds you've provided"
+    ds.close()
+
+### test functions for Full Disk images ###
+
+def read_test_data() -> list:   
+    filepath = './resources/spestana-goes-ortho-data-bc4e02e/data/F/'
+    bounds = [-40, 40, -170, 20]
+    newfilepath = path.abspath('./tmp.nc')
+    goes_clip.subsetNetCDF(filepath,bounds,newfilepath)
+    
+    return [H, req, rpol, e, lon_0_deg]
+
+def test_goes_clip_fulldisk():
+    # run goes clip function
+    goes_clip_fulldisk()
+    # do we have a dataset?
+    assert type(xr.open_dataset('tmp.nc')) == xr.core.dataset.Dataset, "unable to open file"
+    ds = xr.open_dataset('tmp.nc')
+    # does it have non-zero dimensions?
+    assert ds.dims['y'] != 0, "y dimension should be non-zero, check the latitude bounds you've provided"
+    assert ds.dims['x'] != 0, "x dimension should be non-zero, check the longitude bounds you've provided"
+    ds.close()
+
+get_data.download_example_data()
+
+test_goes_clip_conus()
+
+test_goes_clip_fulldisk()
