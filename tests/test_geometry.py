9cb56c81271cddb07104df21fc1f6e36690c796b
Steven Pestana <650301+spestana@users.noreply.github.com>
2024-03-15 12:30:52 -0700

    Update test_geometry.py

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -4,8 +4,7 @@ from os import path
 import sys
 from pathlib import Path
 import xarray as xr
-from goes_ortho import goes_geometry
-from goes_ortho import get_data
+import goes_ortho as go
 
 
 def test_LonLat2ABIangle(setup_session):
@@ -41,51 +40,3 @@ def test_both(setup_session):
     # allowing a very small difference (1e-9) due to rounding errors/precision limits in the functions
     assert abs(lon - original_lon) < 1e-9, "Longitude does not match original longitude after conversions"
     assert abs(lat - original_lat) < 1e-9, "Latitude does not match original latitude after conversions"
-
-
-
-### test functions for CONUS images ###
-
-def goes_clip_conus():
-    filepath = './resources/spestana-goes-ortho-data-bc4e02e/data/C/'
-    bounds = [30, 40, -110, -100]
-    newfilepath = path.abspath('./tmp.nc')
-    goes_clip.subsetNetCDF(filepath,bounds,newfilepath)
-
-def test_goes_clip_conus():
-    # run goes clip function
-    goes_clip_conus()
-    # do we have a dataset?
-    assert type(xr.open_dataset('tmp.nc')) == xr.core.dataset.Dataset, "unable to open file"
-    ds = xr.open_dataset('tmp.nc')
-    # does it have non-zero dimensions?
-    assert ds.dims['y'] != 0, "y dimension should be non-zero, check the latitude bounds you've provided"
-    assert ds.dims['x'] != 0, "x dimension should be non-zero, check the longitude bounds you've provided"
-    ds.close()
-
-### test functions for Full Disk images ###
-
-def read_test_data() -> list:   
-    filepath = './resources/spestana-goes-ortho-data-bc4e02e/data/F/'
-    bounds = [-40, 40, -170, 20]
-    newfilepath = path.abspath('./tmp.nc')
-    goes_clip.subsetNetCDF(filepath,bounds,newfilepath)
-    
-    return [H, req, rpol, e, lon_0_deg]
-
-def test_goes_clip_fulldisk():
-    # run goes clip function
-    goes_clip_fulldisk()
-    # do we have a dataset?
-    assert type(xr.open_dataset('tmp.nc')) == xr.core.dataset.Dataset, "unable to open file"
-    ds = xr.open_dataset('tmp.nc')
-    # does it have non-zero dimensions?
-    assert ds.dims['y'] != 0, "y dimension should be non-zero, check the latitude bounds you've provided"
-    assert ds.dims['x'] != 0, "x dimension should be non-zero, check the longitude bounds you've provided"
-    ds.close()
-
-get_data.download_example_data()
-
-test_goes_clip_conus()
-
-test_goes_clip_fulldisk()
